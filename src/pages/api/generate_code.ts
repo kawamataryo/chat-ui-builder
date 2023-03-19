47138e08021d2883a0db00adf5658711aa1cd4ef
kawamataryo <ba068082@gmail.com>
2023-03-19 21:05:05 +0900

    [from now] 2023/03/19 21:05:05
    diff --git a/src/pages/api/generate_code.ts b/src/pages/api/generate_code.ts
    index e087d2f..9236822 100644
    --- a/src/pages/api/generate_code.ts
    +++ b/src/pages/api/generate_code.ts
    @@ -30,43 +30,43 @@ export default async function handler(
     ) {
       if (req.method === 'POST') {
         try {
    -    const { prompt, chatHistory, apiKey, locale } = req.body;
    -    const safeApiKey = apiKey || process.env.OPEN_AI_API_KEY;
    -    const isJa = locale === 'ja';
    -    const localeTexts = isJa ? JaLocale : EnLocale;
    -    const systemPrompt = isJa ? SYSTEM_PROMPT.JA : SYSTEM_PROMPT.EN;
    +      const { prompt, chatHistory, apiKey, locale } = req.body;
    +      const safeApiKey = apiKey || process.env.OPEN_AI_API_KEY;
    +      const isJa = locale === 'ja';
    +      const localeTexts = isJa ? JaLocale : EnLocale;
    +      const systemPrompt = isJa ? SYSTEM_PROMPT.JA : SYSTEM_PROMPT.EN;
    
    -    if(!safeApiKey) {
    -      res.status(200).json({ content: localeTexts.api.apiKeyNotSet })
    -    }
    +      if(!safeApiKey) {
    +        res.status(200).json({ content: localeTexts.api.apiKeyNotSet })
    +      }
    
    -    const configuration = new Configuration({
    -      apiKey: safeApiKey
    -    });
    -    const openai = new OpenAIApi(configuration);
    +      const configuration = new Configuration({
    +        apiKey: safeApiKey
    +      });
    +      const openai = new OpenAIApi(configuration);
    
    -    const completion = await openai.createChatCompletion({
    -      model: "gpt-3.5-turbo",
    -      messages: [
    -        { role: "system", content: systemPrompt },
    -        ...chatHistory,
    -        { role: "user", content: `${localeTexts.api.apiKeyNotSet}: ${prompt}` }
    -      ]
    -    });
    -    const assistantResponse = completion.data.choices[0].message?.content;
    +      const completion = await openai.createChatCompletion({
    +        model: "gpt-3.5-turbo",
    +        messages: [
    +          { role: "system", content: systemPrompt },
    +          ...chatHistory,
    +          { role: "user", content: `${localeTexts.api.apiKeyNotSet}: ${prompt}` }
    +        ]
    +      });
    +      const assistantResponse = completion.data.choices[0].message?.content;
    
    -    // コードブロックを抽出
    -    const codeBlockPattern = /```html\n([\s\S]+?)\r?\n```/
    -    const matchResult = assistantResponse?.match(codeBlockPattern)
    +      // コードブロックを抽出
    +      const codeBlockPattern = /```html\n([\s\S]+?)\r?\n```/
    +      const matchResult = assistantResponse?.match(codeBlockPattern)
    
    -    if (matchResult && matchResult.length > 1) {
    -      res.status(200).json({ content: assistantResponse, code: matchResult[1] })
    -    } else {
    -      res.status(200).json({ content: assistantResponse})
    +      if (matchResult && matchResult.length > 1) {
    +        res.status(200).json({ content: assistantResponse, code: matchResult[1] })
    +      } else {
    +        res.status(200).json({ content: assistantResponse})
    +      }
    +    } catch (error) {
    +      res.status(500).json({ content: "", hasError: true, errorMessage: "Error: Server Error" });
         }
    -  } catch (error) {
    -    res.status(500).json({ content: "", hasError: true, errorMessage: "Error: Server Error" });
    -  }
       } else {
         res.status(405).json({ content: "", hasError: true, errorMessage: "Error: Bad request" });
       }

--- src/pages/api/generate_code.ts
+++ src/pages/api/generate_code.ts
@@ -30,43 +30,43 @@ export default async function handler(
 ) {
   if (req.method === 'POST') {
     try {
-    const { prompt, chatHistory, apiKey, locale } = req.body;
-    const safeApiKey = apiKey || process.env.OPEN_AI_API_KEY;
-    const isJa = locale === 'ja';
-    const localeTexts = isJa ? JaLocale : EnLocale;
-    const systemPrompt = isJa ? SYSTEM_PROMPT.JA : SYSTEM_PROMPT.EN;
+      const { prompt, chatHistory, apiKey, locale } = req.body;
+      const safeApiKey = apiKey || process.env.OPEN_AI_API_KEY;
+      const isJa = locale === 'ja';
+      const localeTexts = isJa ? JaLocale : EnLocale;
+      const systemPrompt = isJa ? SYSTEM_PROMPT.JA : SYSTEM_PROMPT.EN;
 
-    if(!safeApiKey) {
-      res.status(200).json({ content: localeTexts.api.apiKeyNotSet })
+      if(!safeApiKey) {
+        res.status(200).json({ content: localeTexts.api.apiKeyNotSet })
+      }
+
+      const configuration = new Configuration({
+        apiKey: safeApiKey
+      });
+      const openai = new OpenAIApi(configuration);
+
+      const completion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [
+          { role: "system", content: systemPrompt },
+          ...chatHistory,
+          { role: "user", content: `${localeTexts.api.apiKeyNotSet}: ${prompt}` }
+        ]
+      });
+      const assistantResponse = completion.data.choices[0].message?.content;
+
+      // コードブロックを抽出
+      const codeBlockPattern = /```html\n([\s\S]+?)\r?\n```/
+      const matchResult = assistantResponse?.match(codeBlockPattern)
+
+      if (matchResult && matchResult.length > 1) {
+        res.status(200).json({ content: assistantResponse, code: matchResult[1] })
+      } else {
+        res.status(200).json({ content: assistantResponse})
+      }
+    } catch (error) {
+      res.status(500).json({ content: "", hasError: true, errorMessage: "Error: Server Error" });
     }
-
-    const configuration = new Configuration({
-      apiKey: safeApiKey
-    });
-    const openai = new OpenAIApi(configuration);
-
-    const completion = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages: [
-        { role: "system", content: systemPrompt },
-        ...chatHistory,
-        { role: "user", content: `${localeTexts.api.apiKeyNotSet}: ${prompt}` }
-      ]
-    });
-    const assistantResponse = completion.data.choices[0].message?.content;
-
-    // コードブロックを抽出
-    const codeBlockPattern = /```html\n([\s\S]+?)\r?\n```/
-    const matchResult = assistantResponse?.match(codeBlockPattern)
-
-    if (matchResult && matchResult.length > 1) {
-      res.status(200).json({ content: assistantResponse, code: matchResult[1] })
-    } else {
-      res.status(200).json({ content: assistantResponse})
-    }
-  } catch (error) {
-    res.status(500).json({ content: "", hasError: true, errorMessage: "Error: Server Error" });
-  }
   } else {
     res.status(405).json({ content: "", hasError: true, errorMessage: "Error: Bad request" });
   }
