99f7cd64253104ba1b5491c1196e8670a38581c4
statfoxbeats <122543507+statfoxbeats@users.noreply.github.com>
2023-03-19 13:19:34 -0500

    Update Header.tsx

--- src/conmponents/Header.tsx
+++ src/conmponents/Header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
     <div className="mt-5 text-white">
       <div className="flex text-4xl items-center gap-2 flex-col sm:flex-row">
         <span className="flex items-center gap-2 font-bold">
-          <Image src="/logo1.png" width={125} height={55} className="inline-block" alt=""/>
+          <Image src="/newlogo.png" className="inline-block" alt=""/>
           Speak With God Now!
         </span>
       </div>
