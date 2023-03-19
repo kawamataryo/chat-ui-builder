1fe93ad42ed9c086481cb7705057f79361b7609f
statfoxbeats <122543507+statfoxbeats@users.noreply.github.com>
2023-03-19 08:02:07 -0500

    Update Information.tsx

--- src/conmponents/Information.tsx
+++ src/conmponents/Information.tsx
@@ -13,33 +13,14 @@ const Information = ({ apiKey, setApiKey, t }: Props) => {
         </p>
         <p className="mt-3">
           <a
-            href="https://github.com/kawamataryo/chat-builder"
+            href=""
             className="link link-info"
           >
-            kawamataryo/chat-builder
+           
           </a>
         </p>
       </div>
-      <div>
-        <p className="text-2xl font-bold">🛠️ Setting</p>
-        <p className="mt-2">
-          {t('information.setting.description')}
-        </p>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text text-gray-50">
-              API Key
-              <span className="text-xs text-gray-400 ml-3">
-                ※ {t('information.setting.notice')}
-              </span>
-            </span>
-          </label>
-          <input
-            value={apiKey}
-            onChange={(e) => setApiKey(e.target.value)}
-            type="password"
-            className="input input-bordered input-sm !bg-gray-100 !text-gray-900  max-w-[300px]"
-            placeholder="*****************"
+     
           />
         </div>
       </div>
