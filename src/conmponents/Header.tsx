5494a74c0d98a457b189e39633cfc71f296a3c42
kawamataryo <ba068082@gmail.com>
2023-03-19 14:37:17 +0900

    [from now] 2023/03/19 14:37:17
    diff --git a/src/conmponents/Header.tsx b/src/conmponents/Header.tsx
    index 6352231..1207c6e 100644
    --- a/src/conmponents/Header.tsx
    +++ b/src/conmponents/Header.tsx
    @@ -1,14 +1,16 @@
    +import Image from 'next/image'
    +
     const Header = () => {
       return (
         <div className="mt-5 text-white">
           <div className="flex text-4xl items-center gap-2 flex-col sm:flex-row">
             <span className="flex items-center gap-2 font-bold w-[220px]">
    -          <img src="/ChatGPT.png" width="50" className="inline-block" />
    +          <Image src="/ChatGPT.png" width={50} height={50} className="inline-block" alt=""/>
               ChatGPT
             </span>
             <span>×</span>
             <span className="flex items-center gap-2 font-bold w-[220px]">
    -          <img src="/StackBlitz.png" width="50" className="inline-block" />
    +          <Image src="/StackBlitz.png" width={50} height={50} className="inline-block" alt=""/>
               StackBlitz
             </span>
           </div>

--- src/conmponents/Header.tsx
+++ src/conmponents/Header.tsx
@@ -1,14 +1,16 @@
+import Image from 'next/image'
+
 const Header = () => {
   return (
     <div className="mt-5 text-white">
       <div className="flex text-4xl items-center gap-2 flex-col sm:flex-row">
         <span className="flex items-center gap-2 font-bold w-[220px]">
-          <img src="/ChatGPT.png" width="50" className="inline-block" />
+          <Image src="/ChatGPT.png" width={50} height={50} className="inline-block" alt=""/>
           ChatGPT
         </span>
         <span>×</span>
         <span className="flex items-center gap-2 font-bold w-[220px]">
-          <img src="/StackBlitz.png" width="50" className="inline-block" />
+          <Image src="/StackBlitz.png" width={50} height={50} className="inline-block" alt=""/>
           StackBlitz
         </span>
       </div>
